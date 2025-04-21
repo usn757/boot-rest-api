623c40583d65184aaa690fd2c152cfb1113e4a15
usn757 <usn757@gmail.com>
2025-04-21 15:50:22 +0900

    first commit

--- src/main/java/org/example/bootrestapi/model/entity/Food.java
+++ src/main/java/org/example/bootrestapi/model/entity/Food.java
@@ -1,4 +1,17 @@
 package org.example.bootrestapi.model.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
